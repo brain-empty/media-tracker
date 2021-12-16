9740a28bfd32d452ca94167f35c8aa01dc10a52e
brain-empty <kevingomes1211@gmail.com>
2021-12-16 16:21:31 +0530

    finished working on user show pages and backlog pages

--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,7 @@ const mongoose = require ('mongoose');
 
 router.get ('/:id', async (req,res) => {
     try {
-        const user = await User.findById(req.params.id).populate('movies.movie books.book').exec();
+        const user = await User.findById(req.params.id).populate('movies.movie books.book shows.show').exec();
         let passObj = { user:user }
         if (req.user) {passObj.username=req.user.username}
         res.render ('user/show', passObj)
@@ -24,6 +24,20 @@ router.get ('/:id/movies', async (req,res) => {
     let passObj = { user:user }
     if (req.user) {passObj.username=req.user.username}
     res.render ('user/showmovies', passObj)
+})
+
+router.get ('/:id/books', async (req,res) => {
+    const user = await User.findById(req.params.id).populate('books.book').exec();
+    let passObj = { user:user }
+    if (req.user) {passObj.username=req.user.username}
+    res.render ('user/showbooks', passObj)
+})
+
+router.get ('/:id/shows', async (req,res) => {
+    const user = await User.findById(req.params.id).populate('shows.show').exec();
+    let passObj = { user:user }
+    if (req.user) {passObj.username=req.user.username}
+    res.render ('user/showshow', passObj)
 })
 
 module.exports = router;
